1b7d83bdbd9d996978a1a0fc0e759b85f921d9b1
Tobias Jülg <tobias.juelg@utn.de>
2026-04-08 10:34:24 +0200

    fix(core): add constructors for grp config, state and robot config

--- src/pybind/rcs.cpp
+++ src/pybind/rcs.cpp
@@ -372,9 +372,9 @@ PYBIND11_MODULE(_core, m) {
       .def_readwrite("tcp_offset", &rcs::sim::SimRobotConfig::tcp_offset)
       .def_readwrite("robot_platform",
                      &rcs::common::RobotConfig::robot_platform);
-  py::class_<rcs::common::RobotState>(common, "RobotState");
-  py::class_<rcs::common::GripperConfig>(common, "GripperConfig");
-  py::class_<rcs::common::GripperState>(common, "GripperState");
+  py::class_<rcs::common::RobotState>(common, "RobotState").def(py::init<>());
+  py::class_<rcs::common::GripperConfig>(common, "GripperConfig").def(py::init<>());
+  py::class_<rcs::common::GripperState>(common, "GripperState").def(py::init<>());
   py::enum_<rcs::common::GraspType>(common, "GraspType")
       .value("POWER_GRASP", rcs::common::GraspType::POWER_GRASP)
       .value("PRECISION_GRASP", rcs::common::GraspType::PRECISION_GRASP)
